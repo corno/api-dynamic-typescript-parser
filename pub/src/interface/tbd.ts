f74efd80ef115b5db6d951ae670bdb5dc1855d57
corno <corno.astn.a@schraverus.com>
2022-09-04 09:49:17 +0200

    .

--- pub/src/interface/tbd.ts
+++ pub/src/interface/tbd.ts
@@ -2,31 +2,39 @@ import * as pt from "pareto-core-types"
 
 import * as uast from "api-untyped-ast"
 
-export type File = {
+export type TFile = {
     fullPath: string
     root: uast.TUntypedNode
 }
 
-export type Location = {
+export type TLocation = {
     line: number
     column: number
 }
 
-export type TypeScriptParserError =
+export type TTypeScriptParserError =
     | ["tsconfg.json does not exist", {}]
 
-export type Path = pt.Nested<string>
+export type TPath = pt.Nested<string>
 
-export type Parse = (
+export type XParse = (
     $: {
-        tsconfigPath: Path
+        tsconfigPath: TPath
     },
     $i: {
-        onError: ($: TypeScriptParserError) => void
+        onError: ($: TTypeScriptParserError) => void
         onFile: ($: {
             path: string,
-            data: File
+            data: TFile
         }) => void
         onEnd: () => void
     }
 ) => pt.AsyncNonValue
+
+
+/**
+ * The parser does not strip the quotes of a StringLiteral, but that needs to be done somewhere
+ * As this wrapper is agnostic about StringLiterals, it cannot do it, but it can provide a function that can be used by a consumer
+ * 
+ */
+export type FStripQuotes = ($: string) => string
